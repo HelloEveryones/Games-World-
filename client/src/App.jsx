884003b19374056493875f8dfa85d43fb0139bd8
Iva <iva_dkostadinova@abv.bg>
2024-10-07 10:39:20 +0300

    Add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
-import Header from "./components/header/Header"
+import {Routes, Route } from 'react-router-dom';
 
+import Header from "./components/header/Header";
+import HomePage from './components/homePage/HomePage';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
+import GameList from './components/gameList/GameList';
+import GameCreate from './components/gameCreate/GameCreate';
 
 function App() {
   
@@ -8,9 +14,15 @@ function App() {
       <Header/>
 
       <main id="main-content">
-
+<Routes>
+     <Route path='/' element={<HomePage />} />
+     <Route path='/login' element={<Login />} />
+     <Route path='/register' element={<Register />} />
+     <Route path='/games' element={<GameList />} />
+     <Route path='/games/create' element={<GameCreate />} />
+</Routes>
         </main>
-        
+
       </div>
   )
 }
