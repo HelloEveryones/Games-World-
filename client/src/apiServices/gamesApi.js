ad577d7ec572aafe32b29f485b354125cce1ae25
Iva <iva_dkostadinova@abv.bg>
2024-10-09 14:57:50 +0300

    list all games

--- client/src/apiServices/gamesApi.js
+++ client/src/apiServices/gamesApi.js
@@ -2,4 +2,11 @@ import * as request from './requester';
 
 const BASE_URL = 'http://localhost:3030/jsonstore/games';
 
-export const getAll= async ()=> request.get(BASE_URL);
+export const getAll= async ()=>{
+ const result=await request.get(BASE_URL);
+
+const games= Object.values(result);
+
+return games;
+}
+    
--- client/src/components/gameList/GameList.jsx
+++ client/src/components/gameList/GameList.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
 import * as gamesApi from '../../apiServices/gamesApi'
+import GameListItem from './gameListItem/GameListItem';
 
 export default function GameList(){
-    const [ games, setGames]=useState([])
+    const [ games, setGames]= useState([])
 
 useEffect(()=>{
 gamesApi.getAll()
@@ -14,32 +15,8 @@ gamesApi.getAll()
         <section id="catalog-page">
             <h1>All Games</h1>
             {/*<!-- Display div: with information about every game (if any) -->*/}
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg"/>
-                    <h6>Action</h6>
-                    <h2>Cover Fire</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg"/>
-                    <h6>Action</h6>
-                    <h2>Zombie lang</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-
-            </div>
-            <div className="allGames">
-                <div className="allGames-info">
-                    <img src="./images/avatar-1.jpg"/>
-                    <h6>Action</h6>
-                    <h2>MineCraft</h2>
-                    <a href="#" className="details-button">Details</a>
-                </div>
-            </div>
+{games.map(game=><GameListItem key={game._id} {...game}/>)}
+          
 
             {/*<!-- Display paragraph: If there is no games  -->*/}
             <h3 className="no-articles">No articles yet</h3>
