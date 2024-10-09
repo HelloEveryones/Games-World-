f9de61725616ef497d59900bbc352410944d0981
Iva <iva_dkostadinova@abv.bg>
2024-10-09 15:05:07 +0300

    add no games message

--- client/src/components/gameList/GameList.jsx
+++ client/src/components/gameList/GameList.jsx
@@ -15,11 +15,13 @@ gamesApi.getAll()
         <section id="catalog-page">
             <h1>All Games</h1>
             {/*<!-- Display div: with information about every game (if any) -->*/}
-{games.map(game=><GameListItem key={game._id} {...game}/>)}
-          
-
-            {/*<!-- Display paragraph: If there is no games  -->*/}
-            <h3 className="no-articles">No articles yet</h3>
+{games.length > 0}
+? {games.map(game=><GameListItem key={game._id} {...game}/>)}
+: <h3 className="no-articles">No added games yet</h3>
         </section>
     )
 }
+        
+
+            {/*<!-- Display paragraph: If there is no games  -->*/}
+            
